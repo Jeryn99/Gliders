9dc5375e71a75471ec41cc77715b96dfa45c4971
Craig <sandedshoes@gmail.com>
2023-01-11 21:19:13 +0000

    Initial commit

---
+++ common/src/main/java/net/examplemod/ExampleMod.java
@@ -0,0 +1,10 @@
+package net.examplemod;
+
+public class ExampleMod {
+
+    public static final String MOD_ID = "examplemod";
+
+    public static void init() {
+        // Mod initialization
+    }
+}
