57424c52b71fe80976362a9a086831e3a97f5a30
Craig <sandedshoes@gmail.com>
2023-02-09 16:43:33 +0000

    Recipe start

--- common/src/main/java/net/venturecraft/gliders/VCGlidersClient.java
+++ common/src/main/java/net/venturecraft/gliders/VCGlidersClient.java
@@ -32,6 +32,7 @@ public class VCGlidersClient {
                         if (GliderItem.hasCopperUpgrade(itemStack) && GliderItem.hasNetherUpgrade(itemStack)) {
                             return 0.1F;
                         }
+
                         if (GliderItem.hasCopperUpgrade(itemStack)) {
                             return 0.2F;
                         }
