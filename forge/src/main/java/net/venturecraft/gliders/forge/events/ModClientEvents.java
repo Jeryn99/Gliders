43dabafd34ff066a4d821e69769d833bb3de0919
Craig <sandedshoes@gmail.com>
2023-03-05 13:13:21 +0000

    Fixes Forge server crash

---
+++ forge/src/main/java/net/venturecraft/gliders/forge/events/ModClientEvents.java
@@ -0,0 +1,18 @@
+package net.venturecraft.gliders.forge.events;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.TextureStitchEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.venturecraft.gliders.VCGliders;
+
+@Mod.EventBusSubscriber(value = Dist.CLIENT, modid = VCGliders.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModClientEvents {
+
+    @SubscribeEvent
+    public void texStitch(TextureStitchEvent.Pre textureStitchEvent){
+        textureStitchEvent.addSprite(new ResourceLocation(VCGliders.MOD_ID, "item/glider_slot"));
+    }
+
+}
