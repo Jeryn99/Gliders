f56d01c23f2ae01038b03c2caa43d253917605e3
Cassian Godsted <cassian@godsted.com>
2025-07-04 16:20:03 +0100

    Various glider item fixes (#107)

--- changelog.md
+++ changelog.md
@@ -1,20 +1,12 @@
-# Gliders v1.1.7 – Minecraft 1.20.1
+# Gliders v1.1.8 – Minecraft 1.20.1
 
 ### ✨ What's New
 
-- 🚀 **Improved Performance**
-- Optimized updraft particles for better FPS and smoother gameplay.
-
-- 🛠️ **Bug Fixes**
-- 🧰 Resolved overlapping config button issue.
-_(Closes [#87](https://github.com/Jeryn99/Gliders/issues/87))_
-- 🔥 Fixed hot blocks incorrectly creating updrafts when covered.
-_(Closes [#86](https://github.com/Jeryn99/Gliders/issues/86))_
-- 🪂 Fixed issue preventing players from dismounting while using a glider.
-_(Closes [#81](https://github.com/Jeryn99/Gliders/issues/81))_
-
-- 🎵 **Audio Improvements**
-- Gliders now play an equip sound when worn.
-_(Closes [#83](https://github.com/Jeryn99/Gliders/issues/83))_
+🛠️ **Bug Fixes**
+- Fix issue with Title Screen Mobs ([#118](https://github.com/Jeryn99/Gliders/pull/118))
+- Fixes player reported modpack crash ([#91](https://github.com/Jeryn99/Gliders/pull/91))
+- Curio slot texture is black ([#127](https://github.com/Jeryn99/Gliders/pull/127))
+- Corrected some language entries ([#125](https://github.com/Jeryn99/Gliders/pull/125))
+- gliders still render in spectator ([#121](https://github.com/Jeryn99/Gliders/pull/121))
 
 Happy gliding! 🪁
--- common/src/main/java/net/venturecraft/gliders/common/item/GliderItem.java
+++ common/src/main/java/net/venturecraft/gliders/common/item/GliderItem.java
@@ -8,36 +8,31 @@ import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Equipable;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
-import net.threetag.palladiumcore.item.IPalladiumItem;
 import net.venturecraft.gliders.util.ModConstants;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.function.Supplier;
 
-public class GliderItem extends Item implements IPalladiumItem, Equipable {
+public class GliderItem extends Item implements Equipable {
 
     private final Supplier<ItemStack> repair;
 
     public GliderItem(Properties itemProperties, Supplier<ItemStack> stackSupplier) {
         super(itemProperties);
         this.repair = stackSupplier;
+        DispenserBlock.registerBehavior(this, ArmorItem.DISPENSE_ITEM_BEHAVIOR);
     }
 
     public static boolean isSpaceGlider(ItemStack stack) {
         return stack.getDisplayName().getString().contains("xwing");
     }
-
-
-
 
     public static ItemStack setCopper(ItemStack itemStack, boolean copper) {
         CompoundTag compound = itemStack.getOrCreateTag();
@@ -127,23 +122,9 @@ public class GliderItem extends Item implements IPalladiumItem, Equipable {
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand interactionHand) {
-        ItemStack itemstack = player.getItemInHand(interactionHand);
-        EquipmentSlot equipmentslot = Mob.getEquipmentSlotForItem(itemstack);
-        ItemStack equipmentSlotStack = player.getItemBySlot(equipmentslot);
-        if (equipmentSlotStack.isEmpty()) {
-            player.setItemSlot(equipmentslot, itemstack.copy());
-            if (!level.isClientSide()) {
-                player.awardStat(Stats.ITEM_USED.get(this));
-            }
-
-            itemstack.setCount(0);
-            return InteractionResultHolder.sidedSuccess(itemstack, level.isClientSide());
-        } else {
-            return InteractionResultHolder.fail(itemstack);
-        }
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+        return this.swapWithEquipmentSlot(this, level, player, usedHand);
     }
-
 
     @Override
     public EquipmentSlot getEquipmentSlot() {
